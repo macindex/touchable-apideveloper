b5b451fec19e69bf0f48a3d3a0f41d76d5414903
marcio lira <machollanda@gmail.com>
2020-11-11 09:59:14 -0300

    array filter

---
+++ modulo-2/04-lists/for-forin/3.filter.js
@@ -0,0 +1,52 @@
+// Filtrar as informações
+
+const {obterPessoas} = require('./service')
+// Quero que vc extraia somente o obterPessoas
+
+Array.prototype.meuFilter = function (callback){
+    
+    const lista = []
+
+    for(index in this){
+        const item = this[index]
+        const result = callback(item, index, this)
+        // 0, "", "null, undefined === false"
+        if (!result) continue;
+        lista.push(item)
+    }
+    return lista;
+}
+
+
+async function main (){
+	try{
+        const { results } = await obterPessoas(`a`)
+        
+        // const familiaLars = results.filter(function (item){
+            // por padrão precisa retornar um boolean
+            // para informar se deve manter ou remover da lista
+            // false > remover da lista
+            // true > mantem
+            // não encontrou = -1
+            // encontrou = posicaiNoArray
+
+            // const result = item.name.toLowerCase().indexOf(`lars`) !== -1
+            // return result
+
+
+        
+        const familiaLars = results.meuFilter(item, index, lista) => {
+            console.log(`index: ${index}`, lista.length)
+            return item.name.toLowerCase().indexOf('lars') !== 1
+        })
+        
+        const names = familiaLars.map((pessoa) => pessoa.name)
+        console.log(names)
+
+    }catch (error){
+        console.error('DEU RUIM', error)
+    }
+	
+}
+
+
